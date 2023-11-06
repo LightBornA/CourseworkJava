a6faeb2dc180415a8d9c222fb181b1504985a3c5
AriaGod <alexbes2k16@gmail.com>
2023-11-06 17:35:57 +0300

    2 layouts, stuff

---
+++ app/src/main/java/com/mirea/kt/android/bessonowcwfinal/IpCalcActivity.java
@@ -0,0 +1,14 @@
+package com.mirea.kt.android.bessonowcwfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IpCalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ip_calc);
+    }
+}
