5247e4bc2e4fdb487e3bf6782728014587c6b254
AriaGod <alexbes2k16@gmail.com>
2023-11-06 22:51:57 +0300

    IPCALC DONE

--- app/src/main/java/com/mirea/kt/android/bessonowcwfinal/IpCalcActivity.java
+++ app/src/main/java/com/mirea/kt/android/bessonowcwfinal/IpCalcActivity.java
@@ -1,14 +1,44 @@
 package com.mirea.kt.android.bessonowcwfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.mirea.kt.android.bessonowcwfinal.IPv4;
 
 public class IpCalcActivity extends AppCompatActivity {
+    Button CalculateButton;
+    EditText EditIp;
+    EditText EditMask;
+    TextView OutputText;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ip_calc);
+        CalculateButton = (Button) findViewById(R.id.ButtonFindNetwork);
+        EditIp = (EditText) findViewById(R.id.ipedit) ;
+        EditMask = (EditText) findViewById(R.id.maskedit) ;
+        OutputText = (TextView) findViewById(R.id.networkadresout);
+
+
+        CalculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IPv4 Calculator = new IPv4(EditIp.getText().toString(), EditMask.getText().toString());
+                OutputText.setText(Calculator.getCIDR());
+
+
+
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/mirea/kt/android/bessonowcwfinal/SelectorActivity.java
+++ app/src/main/java/com/mirea/kt/android/bessonowcwfinal/SelectorActivity.java
@@ -2,13 +2,26 @@ package com.mirea.kt.android.bessonowcwfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectorActivity extends AppCompatActivity {
+    Button ButtonIp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
+        ButtonIp = (Button) findViewById(R.id.ButtonIP);
+        ButtonIp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectorActivity.this, IpCalcActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
